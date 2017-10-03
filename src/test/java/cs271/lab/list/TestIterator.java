9751f40412d16495a72e3b3ff67d087044d05b47
linmaliakal <lmaliakal@luc.edu>
2017-10-03 02:42:46 +0000

    finished TODO & fixed expected values, used Iterator and while loop
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -20,7 +20,8 @@ public class TestIterator {
   @Before
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
-    // TODO also try with a LinkedList - does it make any difference?
+    // DONE also try with a LinkedList - does it make any difference?
+    //list = new LinkedList<Integer>();
   }
 
   @After
@@ -74,13 +75,16 @@ public class TestIterator {
     final Iterator<Integer> i = list.iterator();
     while (i.hasNext()) {
       if (i.next() == 77) {
-        i.remove(); // TODO what happens if you use list.remove(77)?
+        i.remove(); // DONE what happens if you use list.remove(77)?
+        
       }
+      //list.remove(77); 
     }
-    // TODO using assertEquals and Arrays.asList, express which values are left in the list
+    // DONE using assertEquals and Arrays.asList, express which values are left in the list
     // See TestList.java for examples of how to use Arrays.asList; also see the Java Arrays
     // class for more information
-    fail("Not yet implemented"); // remove this line when done
+    list.assertEquals(Arrays.asList(33, 44, 55, 66));
+    //fail("Not yet implemented"); // remove this line when done
   }
 
   @Test
@@ -94,9 +98,15 @@ public class TestIterator {
     list.add(66);
     double sum = 0;
     int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
+    // DONE use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    final Iterator<Integer> x = list.iterator();
+    while (x.hasNext()) {
+      sum = sum + x.next();
+      n = n + 1;
+    }
+    
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
